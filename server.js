4a5850bf8eb6a377d8c483c064f74a4d96f61914
ClownMonster <mk6386223@gmail.com>
2020-08-09 23:58:43 +0530

    v0.0.1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors")
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const path = require('path')
 
 //connect database
@@ -16,6 +16,11 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/job",require("./routes/api/job"));
+
+
+if(process.env.NODE_ENV === "production"){
+ app.use(express.static("client/build")); 
+}
 
 
 
