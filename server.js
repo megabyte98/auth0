bb81981f4781cbdf1ff180e56be295887c82e22f
ClownMonster <mk6386223@gmail.com>
2020-08-10 00:40:50 +0530

    build

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ app.use("/api/job",require("./routes/api/job"));
 
 
 if(process.env.NODE_ENV === "production"){
- app.use(express.static("client/build")); 
+ app.use(express.static("./client/build"));
+
+ app.get("*",(req, res)=>{
+   res.sendFile(path.resolve(__dirname,"./client","build","index.html"));
+ }); 
 }
 
 
